b16657263cc48ea5d4adc8138685a783d6af64e6
adnanshaik <shaikhadnan6608@gmail.com>
2020-09-10 06:32:01 +0530

    Added:Read values from dictionary into Array

--- arith.sh
+++ arith.sh
@@ -1,3 +1,5 @@
+declare -A dict
+k=0
 echo "enter 3 numbers"
 read -p "enter num 1:" a
 read -p "enter num 2:" b
@@ -5,3 +7,16 @@ read -p "enter num 3:" c
 
 result1=$(( a+b*c ))
 result2=$(( a*b+c ))
+result3=$(( c+a/b ))
+result4=$(( a%b+c ))
+
+dict[1]=$result1
+dict[2]=$result2
+dict[3]=$result3
+dict[4]=$result4
+
+for i in ${dict[@]}
+do
+        arr[k]=$i
+        ((k++))
+done
