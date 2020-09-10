c50e30778a20ccfc1f4109d577d79171bfadc5e4
adnanshaik <shaikhadnan6608@gmail.com>
2020-09-10 06:28:43 +0530

    Added:reslts into dictionary

--- arith.sh
+++ arith.sh
@@ -1,3 +1,4 @@
+declare -A dict
 echo "enter 3 numbers"
 read -p "enter num 1:" a
 read -p "enter num 2:" b
@@ -5,3 +6,11 @@ read -p "enter num 3:" c
 
 result1=$(( a+b*c ))
 result2=$(( a*b+c ))
+result3=$(( c+a/b ))
+result4=$(( a%b+c ))
+
+dict[1]=$result1
+dict[2]=$result2
+dict[3]=$result3
+dict[4]=$result4
+
